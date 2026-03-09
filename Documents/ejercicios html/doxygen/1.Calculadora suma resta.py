d342b732e9d910f5e634449e160eee59a226d5d2
flama <rubengalinsogaperez@gmail.com>
2026-03-09 12:47:42 +0100

    ESTE ES DESDE CLASE

---
+++ Documents/ejercicios html/doxygen/1.Calculadora suma resta.py
@@ -0,0 +1,33 @@
+def suma(a, b):
+    """
+    Esta función realizará una suma de dos valores
+
+    parametros:
+        a: esta es la variable a para la suma
+        b: segunda variable para la suma 
+
+    devuelta:
+        devuelve una suma de a y b
+
+    Nota:
+        si se pasan string, se devolverá una concatenación
+    """
+    return a + b
+
+def resta(a, b):
+    """
+    Esta función realizará una suma de dos valores
+
+    parametros:
+        a: esta es la variable a para la resta
+        b: segunda variable para la resta
+
+    devuelta:
+        devuelve una resta de a y b
+
+
+    """
+    return a - b
+
+print(suma(5, 3))
+print(resta(5, 3))
